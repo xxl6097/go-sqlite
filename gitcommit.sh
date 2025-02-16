2abf7b60a7b1ce481483b4cdca9f81a1a4402d6e
beck <beck@example.com>
2025-02-16 15:07:42 +0800

    0.0.7 2025-02-16 15:07:42 by

--- gitcommit.sh
+++ gitcommit.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-function getversion() {
-  appversion=$(cat version.txt)
-  if [ "$appversion" = "" ]; then
-    appversion="0.0.0"
-    echo $appversion
+
+function upgradeVersion() {
+  if [ ! -e version.txt ]; then
+      echo "0.0.0" > version.txt
+  fi
+  version=$(cat version.txt)
+  if [ "$version" = "" ]; then
+    version="0.0.0"
   else
-    v3=$(echo $appversion | awk -F'.' '{print($3);}')
-    v2=$(echo $appversion | awk -F'.' '{print($2);}')
-    v1=$(echo $appversion | awk -F'.' '{print($1);}')
+    v3=$(echo $version | awk -F'.' '{print($3);}')
+    v2=$(echo $version | awk -F'.' '{print($2);}')
+    v1=$(echo $version | awk -F'.' '{print($1);}')
     if [[ $(expr $v3 \>= 99) == 1 ]]; then
       v3=0
       if [[ $(expr $v2 \>= 99) == 1 ]]; then
@@ -19,9 +22,9 @@ function getversion() {
     else
       v3=$(expr $v3 + 1)
     fi
-    ver="$v1.$v2.$v3"
-    echo $ver
+    version="$v1.$v2.$v3"
   fi
+    echo $version > version.txt
 }
 
 function todir() {
@@ -36,66 +39,84 @@ function pull() {
 
 function forcepull() {
   todir
-  echo "git fetch --all && git reset --hard origin/main && git pull"
-  git fetch --all && git reset --hard origin/main && git pull
+  echo "git fetch --all && git reset --hard origin/master && git pull"
+  git fetch --all && git reset --hard origin/$1 && git pull
 }
 
 function tag() {
-    version=$(getversion)
-    echo "current version:${version}"
-    git add .
-    git commit -m "release v${version}"
-    git tag -a v$version -m "release v${version}"
-    git push origin v$version
-    echo $version >version.txt
+  git add .
+  git commit -m "release v${version}"
+  git tag -a v$version -m "release v${version}"
+  git push origin v$version
 }
-#  shellcheck disable=SC2120
-function gitpush() {
+
+function push() {
   commit=""
   if [ ! -n "$1" ]; then
     commit="$(date '+%Y-%m-%d %H:%M:%S') by ${USER}"
   else
     commit="$1 by ${USER}"
   fi
-
   echo $commit
   git add .
-  git commit -m "$commit"
+  git commit -m "${version} $commit"
   #  git push -u origin main
+  echo "提交代码"
   git push
+  echo "打tag标签"
   tag
 }
 
-function test() {
-    version=$(getversion)
-    echo "current version:${version}"
-    echo $version >version.txt
+function main_pre() {
+  #1. 更新版本号
+  upgradeVersion
+}
+
+function utag() {
+    echo "请输入分支名称："
+    read tag
+    forcepull $tag
+}
+
+function forceupdate() {
+    echo "1. master"
+    echo "2. main"
+    echo "3. 输入分支"
+    echo "请输入编号:"
+    read index
+
+    case "$index" in
+    [1]) (forcepull master);;
+    [2]) (pull main);;
+    [3]) (utag);;
+    *) echo "exit" ;;
+  esac
 }
 
 function m() {
     echo "1. 强制更新"
     echo "2. 普通更新"
     echo "3. 提交项目"
-    echo "4. 测试"
     echo "请输入编号:"
     read index
 
     case "$index" in
-    [1]) (forcepull);;
+    [1]) (forceupdate);;
     [2]) (pull);;
-    [3]) (gitpush);;
-    [4]) (test);;
+    [3]) (push);;
     *) echo "exit" ;;
   esac
 }
 
-function bootstrap() {
+function main() {
+  main_pre
     case $1 in
     pull) (pull) ;;
-    m) (m) ;;
+       m) (m) ;;
       -f) (forcepull) ;;
-       *) ( gitpush $1)  ;;
+       *) (push $1)  ;;
     esac
 }
 
-bootstrap m
+main m
+#upgradeVersion
---
+++ pkg/version.go
@@ -0,0 +1,25 @@
+package pkg
+import "fmt"
+var (
+	AppName      string // 应用名称
+	AppVersion   string // 应用版本
+	BuildVersion string // 编译版本
+	BuildTime    string // 编译时间
+	GitRevision  string // Git版本
+	GitBranch    string // Git分支
+	GoVersion    string // Golang信息
+	DisplayName  string // 服务显示名
+	Description  string // 服务描述信息
+)
+// Version 版本信息
+func Version() {
+	fmt.Printf("App Name:\t%s\n", AppName)
+	fmt.Printf("App Version:\t%s\n", AppVersion)
+	fmt.Printf("Build version:\t%s\n", BuildVersion)
+	fmt.Printf("Build time:\t%s\n", BuildTime)
+	fmt.Printf("Git revision:\t%s\n", GitRevision)
+	fmt.Printf("Git branch:\t%s\n", GitBranch)
+	fmt.Printf("Golang Version: %s\n", GoVersion)
+	fmt.Printf("DisplayName:\t%s\n", DisplayName)
+	fmt.Printf("Description: %s\n", Description)
+}
